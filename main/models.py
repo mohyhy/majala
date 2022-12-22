80e3a82fe3226c84de0236a5697e330280cb7645
WAFACo <gh0598386294@gmail.com>
2022-12-22 17:11:44 +0200

    all

--- main/models.py
+++ main/models.py
@@ -6,11 +6,14 @@ class New(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=20,null=True, blank=True)
     image = models.ImageField()
-    date = models.DateField(null=True, blank=True)
+    date1 = models.DateField(null=True, blank=True)
     short_decsription = models.CharField(max_length=100)
     long_decsription = models.TextField(default='DESC')
     most_popular = models.BooleanField(default=False)
     trending = models.BooleanField(default=False)
+
+
+
 
 
 class blog(models.Model):
