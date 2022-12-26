30a51def38a6cdb7463b830c82a2320c5c1bd3db
Motasim T Younis <106841046+motasimyounis@users.noreply.github.com>
2022-12-26 13:41:20 +0200

    Add files via upload

--- main/admin.py
+++ main/admin.py
@@ -4,5 +4,9 @@ from .models import *
 
 class NewAdmin(admin.ModelAdmin):
     list_display = ('id','title','date_post', 'most_popular', 'trending' ,'type' )
-
+    list_editable = ('title', 'most_popular', 'trending' ,'type' )
+    search_fields = ['title']
+    
 admin.site.register(New,NewAdmin)
+admin.site.site_header = 'مجلة الغد'
+admin.site.site_title = 'مجلة الغد'
